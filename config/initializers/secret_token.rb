84ff6f2c36db4612fa020f6b97f62b540358b284
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-04-12 11:16:20 +0000

    Init com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xomster::Application.config.secret_key_base = '59f80576919941f45ef9cc6e8e26d528d4b17e8ea8843cb0deab69fdb709fdad15b2bc82a67d5cbc9f448c77974da872be16475b6d9228cddb598d4e4042b30c'
