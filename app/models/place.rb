b4de6c2cc129d7aeb9a6ac38aab4b29bdb932623
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-04-12 14:52:42 +0000

    No method error here

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ActiveRecord::Base
 	paginates_per 3
 	belongs_to :user
+	geocoded_by :address
+    after_validation :geocode
+
 	validates :name, presence: true, length:{maximum:50,minimum:3}
 	validates :address,presence: true
 	validates :description,presence: true,length:{minimum:5}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412131821) do
+ActiveRecord::Schema.define(version: 20160412140745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
